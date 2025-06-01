12f9a56108e45a01190d81a4100ac38bdb1c7844
itakello <me@mstefan.dev>
2025-06-01 16:30:00 +0200

    Remove test typing violation files

--- test_typing_violations.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Test file to demonstrate typing violations that should be caught by pre-commit."""
-
-from typing import List, Dict, Optional, Union
-
-
-def example_function(items: List[str], mapping: Dict[str, int], value: Optional[str]) -> Union[str, int]:
-    """Example function with old-style typing that should trigger violations."""
-    return "test"
